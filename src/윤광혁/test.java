ab8517ddd09aa7f74dad4f7459820ca2b6139d49
DongJoonLeeDJ <ldjgood12@gmail.com>
2023-06-09 16:39:14 +0900

    RESET

--- src/윤광혁/test.java
+++
@@ -1,7 +0,0 @@
-package 윤광혁;
-
-public class test {
- public static void main(String[] args) {
-	System.out.println("oos");
- }
-}
--- src/정우균/abcdd.java
+++
@@ -1,7 +0,0 @@
-package 정우균;
-
-public class abcdd {
-	public static void main(String[] args) {
-		System.out.println("TEST2332");
-	}
-}
