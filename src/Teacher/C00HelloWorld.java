698596d3805764a94952540741455ef25462c700
jungwoogyun <jwg8910@naver.com>
2023-06-09 16:11:07 +0900

    3333

--- src/Teacher/C00HelloWorld.java
+++ src/Teacher/C00HelloWorld.java
@@ -2,7 +2,7 @@ package Teacher;
 
 public class C00HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HELLO qsedsdWORLD HsdsdsdI MY NAME IS HEON JI LEE");
+		System.out.println("HELLO qsessddsdWORLD HsdsdsdI MY NAME IS HEON JI LEE");
 
 	}
 }
