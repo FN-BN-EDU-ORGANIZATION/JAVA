40889b54c3c8ef3cfbf5fd2f975ac966d0630ce2
Euzeth <knightcy1@gmail.com>
2023-06-09 15:13:26 +0900

    V0.0

--- src/윤치연/C01HELLOWORLD.java
+++ src/윤치연/C01HELLOWORLD.java
@@ -3,5 +3,7 @@ package 윤치연;
 public class C01HELLOWORLD {
 	public static void main(String[] args) {
 		System.out.println("HELLO wORLD");
+		System.out.println("HELLO wORLD");
+		System.out.println("HELLO wORLD");
 	}
 }
