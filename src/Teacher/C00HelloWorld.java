a7890ac76cc7f60c72eca9745836b9fe72c47d1a
YoonGwngHyuck <YGH@YGH-Laptop>
2023-06-09 15:26:27 +0900

    231

--- src/Teacher/C00HelloWorld.java
+++ src/Teacher/C00HelloWorld.java
@@ -2,6 +2,6 @@ package Teacher;
 
 public class C00HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD java");
+		System.out.println("HELLO WORLDs java");
 	}
 }
