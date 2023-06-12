cc4dce2e9b1d4937817ed5e1575489bc4a332221
jungsein <gytls52tpdls@naver.com>
2023-06-12 15:02:44 +0900

    JUNGSEIN_EXAMPLE

---
+++ src/정세인/Star.java
@@ -0,0 +1,31 @@
+package 정세인;
+import java.util.Scanner;
+public class Star {
+	public static void main(String[] args) {
+        Scanner star = new Scanner(System.in);
+        System.out.print("별 높이: ");
+        int height = star.nextInt();
+        int i = 0;
+        
+        while (i < height) {
+            int j = 0;
+            while (j < height - (i + 1)) {
+                System.out.print(" ");
+                j++;
+            }
+            
+            int k = 0;
+            while (k < 2 * i + 1) {
+                System.out.print("*");
+                k++;
+            }
+            
+            System.out.println();
+            i++;
+        }
+    }
+	
+
+	
+
+}
