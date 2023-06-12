6a1f0a11850f240cadf300c71f2d63e398fe9f1c
jaemin <leejm_1994@naver.com>
2023-06-12 10:05:59 +0900

    V0.0 added

---
+++ src/이재민/C01Test.java
@@ -0,0 +1,11 @@
+package 이재민;
+
+public class C01Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
--- src/정우균/C01HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package 정우균;
-
-public class C01HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");
-	}
-
-}
