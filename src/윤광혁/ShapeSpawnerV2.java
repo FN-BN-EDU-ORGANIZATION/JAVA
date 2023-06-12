8ce67ef5fbb34efce5e50b07de5af543d26fb108
YoonGwngHyuck <YGH@YGH-Laptop>
2023-06-12 09:43:33 +0900

    0612

--- src/윤광혁/ShapeSpawnerV2.java
+++ src/윤광혁/ShapeSpawnerV2.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class ShapeSpawnerV2 {
 	//ShapeSpawner의 스타틱으로 지정한거 전부 클레스로 변경할 예정
 	//main에 호출을 제외한 다른 기능은 사용하지 않을 예정
-
+	//높이를 입력받아 적용되게 할 예정
+	//홀수값만 넣을수있게하고 다른 값이 들어오면 만들어둔 오류메시지 출력 (문자도 포함)
 }
