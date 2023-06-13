a2eda7e2bac240584fca368dbd71302a1cf2cfc2
YoonGwngHyuck <YGH@YGH-Laptop>
2023-06-13 13:38:35 +0900

    0613

--- src/윤광혁/ShapeSpawnerV2.java
+++ src/윤광혁/ShapeSpawnerV2.java
@@ -1,12 +1,12 @@
 package 윤광혁;
+
 import java.util.Scanner;
-
-
 
 //메인 부모 클레스 (부모 A)
 class ConsoleMenu{
 	Scanner sc = new Scanner(System.in);
-	public void ConsoleMenu(){
+	String SH; //높이 입력값을 저장 받을 변수
+	public void ConsoleMenu(){ //메인 메뉴 메서드
 		System.out.println("-----------------------------------");
 		System.out.println("- 1 - 직삼각형v1 출력");
 		System.out.println("- 2 - 직삼각형v2 출력");
@@ -16,7 +16,7 @@ class ConsoleMenu{
 		System.out.println("- 6 - 종료");
 		System.out.println("-----------------------------------");
 	}
-	public void lineSpawner() {
+	public void lineSpawner() { //줄 생성기 메서드
 		System.out.println("-----------------------------------");
 	}
 	public void clearConsole() { 				//콘솔창을 정리하는 메서드
@@ -29,8 +29,8 @@ class ConsoleMenu{
 
 class Shapes extends ConsoleMenu{ //도형 생성기를 모아둔 클레스 (자식 B)
 	//직삼각형1
-	static void RectangleV1(){
-		for (int i = 0; i <= 4; i++) {
+	static void RectangleV1(int h){
+		for (int i = 0; i <= h; i++) {
 
 			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
@@ -39,9 +39,10 @@ class Shapes extends ConsoleMenu{ //도형 생성기를 모아둔 클레스 (자
 		}
 	}
 	//직삼각형2
-	static void RectangleV2(){for (int i = 0; i <= 4; i++) {
+	static void RectangleV2(int h){
+		for (int i = 0; i <= h; i++) {
 
-		for (int k = 0; k <= 4 - i; k++) {
+		for (int k = 0; k <= h - i; k++) {
 			System.out.print(" ");
 		}
 		for (int j = 0; j <= i; j++) {
@@ -52,10 +53,10 @@ class Shapes extends ConsoleMenu{ //도형 생성기를 모아둔 클레스 (자
 	}
 	}
 	//정삼각형
-	static void Triangle(){
-		for (int i = 0; i <= 4; i++) {
+	static void Triangle(int h){
+		for (int i = 0; i <= h; i++) {
 
-			for (int k = 0; k <= 4 - i; k++) {
+			for (int k = 0; k <= h - i; k++) {
 				System.out.print(" ");
 			}
 			for (int j = 0; j <= i * 2; j++) {
@@ -66,13 +67,13 @@ class Shapes extends ConsoleMenu{ //도형 생성기를 모아둔 클레스 (자
 		}
 	}
 	//직사각형
-	static void Quadrilateral(){
-		for (int i = 0; i <= 4; i++) {
+	static void Quadrilateral(int h){
+		for (int i = 0; i <= h; i++) {
 
-			for (int k = 0; k <= 4 - i; k++) {
+			for (int k = 0; k <= h - i; k++) {
 				System.out.print(" ");
 			}
-			for (int j = 0; j <= 4; j++) {
+			for (int j = 0; j <= h; j++) {
 				System.out.print("*");
 			}
 
@@ -80,10 +81,10 @@ class Shapes extends ConsoleMenu{ //도형 생성기를 모아둔 클레스 (자
 		}
 	}
 	//마름모
-	static void Rhombus(){
-		for (int i = 0; i <= 4; i++) {
-			if (i <= 4 / 2) {
-				for (int k = 0; k <= 4 - i; k++) {
+	static void Rhombus(int h){
+		for (int i = 0; i <= h; i++) {
+			if (i <= h / 2) {
+				for (int k = 0; k <= (h-2) - i; k++) {
 					System.out.print(" ");
 				}
 				for (int j = 0; j <= i * 2; j++) {
@@ -92,10 +93,10 @@ class Shapes extends ConsoleMenu{ //도형 생성기를 모아둔 클레스 (자
 
 			}
 			else {
-				for (int k = 0; k <= i; k++) {
+				for (int k = 0; k <= i-1; k++) {
 					System.out.print(" ");
 				}
-				for (int j = 0; j <= 8-(i*2); j++) {
+				for (int j = 0; j <= (h-1)*2-(i*2); j++) {
 					System.out.print("*");
 				}
 			}
@@ -108,6 +109,56 @@ class Shapes extends ConsoleMenu{ //도형 생성기를 모아둔 클레스 (자
 	
 }
 
+class ScannerSearcher extends Shapes{ 	
+	boolean outStringSearcher;	//정수,문자 구분 탈출용 변수
+	boolean outintSearcher;		//홀수,짝수 구분 탈출용 변수
+	int intSH;	//정수로 판명된걸 저장하는 변수
+	int OKSH;	//홀수로 판명된걸 저장하는 변수
+	public boolean isint(String str) { //문자인지 정수인지 확인하는 메서드
+		//Integer.parseInt 는 문자를 정수로 변환한다.
+		try { //if를 쓰면 컴파일러가 거부해서 트라이로 강제실행
+			Integer.parseInt(str);	
+			return true;
+			//변환했지만 숫자가 아닌 문자라 뜨는 오류를 받으면 false로
+		} catch (NumberFormatException ex) { 
+			return false;
+		}
+	}
+	public int  scannerSearcher(){		//문자인지 정수인지 홀수인지 짝수인지 확인후 최종값 반환 메서드
+		this.intSH=0;	//변수 초기화
+		this.OKSH=0;
+		this.outStringSearcher = true;
+		System.out.println("높이를 입력");	
+		System.out.print("(주의! 홀수만!) : ");
+		while(outStringSearcher == true) { //문자 인지 정수인지 확인 루프 시작
+		this.SH = sc.next();
+		
+		if(isint(SH)==true) {
+			OKSH = searcherheight(Integer.parseInt(this.SH)); //작수 홀수 확인 메서드 실행 후 반환값 저장
+		}else {
+			System.err.println("정수만 입력하세요!");
+		}
+		
+	}return OKSH;
+	}
+	public int searcherheight(int sh) { //홀수인지 짝수인지 확인하는 메서드
+		this.outintSearcher = true;
+		while(outintSearcher == true) { //홀수인지 짝수인지 확인하는 루프 시작 
+			if(sh%2==1) {
+				this.intSH = sh;
+				this.outintSearcher = false; 	// 홀짝 확인 루프 종료
+				this.outStringSearcher = false; // 문자 정수 확인 루프 종료
+			}else {
+				System.err.println("홀수만 입력하세요!");
+				this.outintSearcher = false; 	//홀짝 확인 루프만 종료시키고 다시 문자 정수 확인 루프로 돌아간다.
+			}									//문자 정수 확인 루프로 돌아가면 문자 정수 확인 루프가 최종값을 리턴하지만
+												//루프가 정지되지는 않아 다시 처음루프로 돌아간다.
+		}
+		return this.intSH;
+		
+	}
+	
+}
 
 
 interface makeShape {//각 도형생성기를 가동시킬 인터페이스
@@ -115,61 +166,61 @@ interface makeShape {//각 도형생성기를 가동시킬 인터페이스
 	}
 
 
-class makeShapeRectangleV1 extends Shapes implements makeShape {
+class makeShapeRectangleV1 extends ScannerSearcher implements makeShape {
+
+	@Override
+	public void makeShape() {
+		clearConsole(); 				//콘솔창 청소기
+		lineSpawner();					//줄 생성기
+		RectangleV1(scannerSearcher());	//도형생성기와 도형생성기에 들어갈 값을 받는 메서드
+		lineSpawner();					//줄 생성기
+		ConsoleMenu();					//메인메뉴
+	}
+	
+}
+class makeShapeRectangleV2 extends ScannerSearcher implements makeShape {
 
 	@Override
 	public void makeShape() {
 		clearConsole();
 		lineSpawner();
-		RectangleV1();
+		RectangleV2(scannerSearcher());
 		lineSpawner();
 		ConsoleMenu();
 	}
 	
 }
-class makeShapeRectangleV2 extends Shapes implements makeShape {
+class makeShapeTriangle extends ScannerSearcher implements makeShape {
 
 	@Override
 	public void makeShape() {
 		clearConsole();
 		lineSpawner();
-		RectangleV2();
+		Triangle(scannerSearcher());
 		lineSpawner();
 		ConsoleMenu();
 	}
 	
 }
-class makeShapeTriangle extends Shapes implements makeShape {
+class makeShapeQuadrilateral extends ScannerSearcher implements makeShape {
 
 	@Override
 	public void makeShape() {
 		clearConsole();
 		lineSpawner();
-		Triangle();
+		Quadrilateral(scannerSearcher());
 		lineSpawner();
 		ConsoleMenu();
 	}
 	
 }
-class makeShapeQuadrilateral extends Shapes implements makeShape {
+class makeShapeRhombus extends ScannerSearcher implements makeShape {
 
 	@Override
 	public void makeShape() {
 		clearConsole();
 		lineSpawner();
-		Quadrilateral();
-		lineSpawner();
-		ConsoleMenu();
-	}
-	
-}
-class makeShapeRhombus extends Shapes implements makeShape {
-
-	@Override
-	public void makeShape() {
-		clearConsole();
-		lineSpawner();
-		Rhombus();
+		Rhombus(scannerSearcher());
 		lineSpawner();
 		ConsoleMenu();
 	}
@@ -244,6 +295,7 @@ public class ShapeSpawnerV2 {
 	//main에 호출을 제외한 다른 기능은 사용하지 않을 예정
 	//높이를 입력받아 적용되게 할 예정
 	//홀수값만 넣을수있게하고 다른 값이 들어오면 만들어둔 오류메시지 출력 (문자도 포함)
+	
 	public static void main(String[] args) {
 		MainRun startshape = new MainRun();
 		startshape.MainRun();
