a68246cfc58925621dc2ce640888afcb5e7b1dd1
jaemin <leejm_1994@naver.com>
2023-06-19 15:10:22 +0900

    ChatGPT_API

---
+++ src/이재민/Main.java
@@ -0,0 +1,43 @@
+package 이재민;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String key = "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";
+		ChatGpt gpt = new ChatGpt(key);
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+			while (true) {
+			gpt.connect();
+			
+				System.out.print("나: ");
+				String message = scanner.nextLine();
+				if (message.equals("대화종료")) {
+					break;
+				}
+				gpt.send(message);
+
+				String response = gpt.receive();
+				// 대화종료조건확인
+
+				// 응답을 출력한다.
+				System.out.println("AI: " + response);
+				
+				gpt.connect().disconnect();
+
+			}
+			
+			
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
