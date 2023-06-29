8873ae8b847bf8189a2af5cd7ba39f27a11e4325
chanj <chanjini7@naver.com>
2023-06-29 15:14:23 +0900

    20230629 - 이찬진

---
+++ src/이찬진/C20230629.java
@@ -0,0 +1,62 @@
+package 이찬진;
+
+import java.util.Scanner;
+
+public class C20230629 {
+
+	public static void main(String[] args) {
+
+
+		//의문점		: 이해안했던 점 적기
+		//정리 : 코드에 대한 전반적인 정리/해석
+		
+		
+		//예제 1
+//		Scanner sc = new Scanner(System.in); //주석
+//		
+//		System.out.println("거리를 입력하세요");
+//		double i = Integer.parseInt(sc.nextLine());  // int와 double의 차이 
+//		double k= 0;								// 정수와 실수를 나타내는 자료형
+//		if(i<1000)
+//		{
+//			System.out.print(i+"m"); 
+//		}else
+//		{
+//			k=i/1000;
+//			System.out.print(k+"km");
+//		}
+//		
+//		//정리 : m가 일정값이 넘어갈 경우 km 로 산정되게하는 정수와 실수를 나타내는 자료형의 차이점
+//		
+//		
+		//예제 2
+		// 주급계산하기 
+		// 근무시간과 시간당 급여액을 입력받아 주급을 계산하시오
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("하루근무시간 : ");
+//		int work_hour = sc.nextInt();
+//		System.out.print("시간당 급여액 : ");
+//		int work_pay = sc.nextInt();
+//		
+//		int week_pay = work_hour * work_pay*7;
+//		System.out.print("주급 : " + week_pay);
+//		
+//		
+//		// 정리 : 스캐너 사용 방법 입력방법
+		
+		//예제 3
+		//사각형의 면적 구하기
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("가로 : ");
+//		int width = sc.nextInt();
+//		System.out.print("세로 : ");
+//		int height = sc.nextInt();
+//		
+//		int Square = width * height;
+//		System.out.print("사각형 넓이 : " + Square);
+//		
+//		
+//		// 정리 : 스캐너 사용 및 입력하여 문제풀기
+	}
+
+}
