d21cacc40b2474d4bf2dc3b9fce7c440fd06023d
chanj <chanjini7@naver.com>
2023-06-30 14:36:48 +0900

    20230630 - 이찬진

---
+++ src/이찬진/C20230630.java
@@ -0,0 +1,89 @@
+package 이찬진;
+
+import java.util.Scanner;
+
+public class C20230630 {
+
+	public static void main(String[] args) {
+		//의문점		: 이해안했던 점 적기
+		//정리 : 코드에 대한 전반적인 정리/해석
+		
+		
+		//예제 1
+		// 삼각형의 세변의 길이를 입력받아 그 삼각형이 직각삼각형인지 여부를 판단하시오
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int a,b,c,temp;
+//		System.out.print("첫번째 : ");
+//		a= sc.nextInt();
+//		System.out.print("두번째 : ");
+//		b= sc.nextInt();
+//		System.out.print("세번째 : ");
+//		c= sc.nextInt();
+//		
+//		if(b>a)
+//		{
+//			temp = a;
+//			a = b;
+//			b = temp;
+//		}
+//		if(c>a)
+//		{
+//			temp = a;
+//			a=c;
+//			c=temp;
+//		}
+//		if(a>(b+c))
+//		{
+//			System.out.println("삼각형이 아닙니다.");
+//			return;
+//		}
+//		if(a*a == b*b +c*c)
+//			System.out.println("직각 삼각형입니다.");
+//		else
+//			System.out.println("직각 삼각형이 아닙니다");
+		
+		
+		
+		//예제 2
+		// 성적처리
+		// 이름,국어,영어,수학 세 과목의 성적을 입력받고 총점과 평균을 구한 후 
+		// 평균이 90점 이상은 수, 80점이상은 우, 70점이상은 미, 60점이상은 양, 나머지는 가로 표시하시오
+//		Scanner sc = new Scanner(System.in);
+//		int Kor,Eng,Math,total;
+//		float avg;
+//		String name, grade;
+//		
+//		System.out.println("학생이름 : ");
+//		name=sc.nextLine();
+//		System.out.println("국어 : ");
+//		Kor = sc.nextInt();
+//		System.out.println("영어 : ");
+//		Eng = sc.nextInt();
+//		System.out.println("수학 : ");
+//		Math = sc.nextInt();
+//		
+//		total = Kor + Eng + Math;
+//		avg = total/3;
+//		
+//		if(avg>=90)
+//			grade = "수";
+//		else if(avg>=80)
+//			grade = "우";
+//		else if(avg>=70)
+//			grade = "미";
+//		else if(avg>=60)
+//			grade = "양";
+//		else
+//			grade = "가";
+//		System.out.println("이름 : " + name);
+//		System.out.println("총점 : " + total);
+//		System.out.println("평균 : " + avg);
+//		System.out.println("등급 : " + grade);
+		
+		
+	
+		
+	}
+
+}
