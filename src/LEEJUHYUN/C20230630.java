3653c721ff1fad25190578dcc4eb703531dfa549
LEEJUHYUN <free21004@naver.com>
2023-06-30 15:03:15 +0900

    6월30일 공부내용

---
+++ src/LEEJUHYUN/C20230630.java
@@ -0,0 +1,29 @@
+package LEEJUHYUN;
+
+public class C20230630 {
+
+	
+	//예제 1
+	// firstMethod(): 숫자들의 합계 구하기 (1부터 10까지의 수)
+//	public static void firstMethod()  
+//	{
+//		int sum = 0;  // sum 변수를 0으로 초기화
+//		
+//		for(int i = 1; i<=10; i++)   // 1부터 10까지의 숫자들을 반복하여 더함
+//			sum += i;
+//	} // 여기서 sum 변수에는 1부터 10까지의 숫자의 합인 55가 저장됩니다.
+//	public static void main(String[] args) {
+//		firstMethod();  // firstMethod() 호출
+//	}
+	
+	//예제 2
+	// firstMethod(int n): 전달받은 정수 값 출력
+//	public static void firstMethod(int n) {
+//		System.out.println("전달받은 값 = " +n);  // 전달받은 정수 n을 출력
+//	}
+//	public static void main (String arg[]) {
+//		firstMethod(50);  // firstMethod() 호출하며 정수 50을 전달
+//						  // 실행 결과: "전달받은 값 = 50" 이 출력됩니다.
+//	}
+
+}
