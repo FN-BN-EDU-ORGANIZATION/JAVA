358fbdec8cbc2c7bbba9e076c7dbe2d9bbb1e311
LEEJUHYUN <free21004@naver.com>
2023-07-03 15:31:17 +0900

    7월3일 공부내용

---
+++ src/LEEJUHYUN/C20230703.java
@@ -0,0 +1,46 @@
+package LEEJUHYUN;
+
+public class C20230703 {
+
+	public static void main(String[] args) {
+		
+		//예제 1
+		// 주사위 번호 뽑기
+//		int num = (int)(Math.random()*6)+1; // 1~6 의 임의의 정수 뽑기
+//		if(num==1) {
+//			System.out.println("1번 주사위");
+//		}else if(num == 2) {
+//			System.out.println("2번 주사위");
+//		}else if(num == 3) {
+//			System.out.println("3번 주사위");
+//		}else if(num == 4) {
+//			System.out.println("4번 주사위");
+//		}else if(num == 5) {
+//			System.out.println("5번 주사위");
+//		}else {
+//			System.out.println("6번 주사위");
+//		}
+		
+		
+		
+		//예제 2
+		//다음 for문을 while문으로 바꿔보시오
+//		for(int dan =2; dan<=9; dan++){
+//		for(int i =1;i<+9; i++){
+//		    System.out.println(dan + "*" + i + "+" + dan*i);
+//		    }
+//		    }
+		
+//		int dan = 2;
+//		while(dan<=9) {
+//			int i = 1;
+//			while(i<=9) {
+//				System.out.println(dan + "*" + i + "+" + dan*i);
+//				i++;
+//			}
+//			dan++;
+//			System.out.println();
+//		}
+	}
+
+}
