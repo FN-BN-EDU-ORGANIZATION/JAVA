4d29c27070d1e3bb6b5963980f4844107e9dd5e6
황다은 <dutlsghkd@naver.com>
2023-07-03 15:35:25 +0900

    V 0.0 day20230703

---
+++ src/황다은/C20230703.java
@@ -0,0 +1,35 @@
+package 황다은;
+
+import java.util.Scanner;
+
+public class C20230703 {
+
+	public static void main(String[] args) {
+		//예제 1
+		//사각형의 면적 구하기
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("가로 : ");
+//		int width = sc.nextInt();
+//		System.out.print("세로 : ");
+//		int height = sc.nextInt();
+//
+//		int Square = width * height;
+//		System.out.print("사각형 넓이 : " + Square);
+
+
+		// 정리 : 스캐너 사용 및 입력하여 문제풀기
+		
+		//예제2
+		//주급계산하기
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("하루근무시간");
+//		int work_hour = Integer.parseInt(sc.nextLine());
+//		System.out.println("시간당 급여액");
+//		int work_pay = Integer.parseInt(sc.nextLine());
+//		
+//		int week_pay = work_hour * work_pay*7;
+//		System.out.println("주급 : " + week_pay);
+
+	}
+
+}
