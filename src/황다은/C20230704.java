33bf4e49bcbf357b49d4c7a8798de157c53b3855
황다은 <dutlsghkd@naver.com>
2023-07-04 15:48:14 +0900

    V 20230704

---
+++ src/황다은/C20230704.java
@@ -0,0 +1,33 @@
+package 황다은;
+
+import java.util.Scanner;
+
+public class C20230704 {
+
+	public static void main(String[] args) {
+		// 예제1
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("가로: ");
+//		int width = Integer.parseInt(sc.nextLine());
+//		System.out.println("세로 : ");
+//		int height = Integer.parseInt(sc.nextLine());
+//		
+//		int Square = width*height;
+//		System.out.println("넓이 : " + Square);
+		//정리 : 사각형의 넓이 구하기
+		
+		//예제2
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("윗면 : ");
+//		int top = Integer.parseInt(sc.nextLine());
+//		System.out.println("아랫면 : ");
+//		int bottom = Integer.parseInt(sc.nextLine());
+//		System.out.println("높이 : ");
+//		int height = Integer.parseInt(sc.nextLine());
+//		
+//		int Square = (top+bottom)*height/2;
+//		System.out.println("넓이 : "+Square);
+
+	}
+
+}
