b73564ec58dfd39e8197a3283e7550ec78cc551f
chanj <chanjini7@naver.com>
2023-07-04 14:42:32 +0900

    20230704 - 이찬진

---
+++ src/이찬진/C20230704.java
@@ -0,0 +1,42 @@
+package 이찬진;
+
+import java.util.Scanner;
+
+//예제1
+		//"안녕하세요 오늘은 비가 온데요"라는 출력문을 보여주는 클래스
+//	
+//class Print{
+//	void showInfo() {
+//		System.out.println("안녕하세요 오늘은 비가 온데요");
+//	}
+//}
+//public class C20230704 {
+//	public static void main(String[] args) {
+//		
+//		Print myPrint = new Print();
+//		myPrint.showInfo();
+//	}
+//}
+
+		//예제2
+		//정수 2개를 전달받아 두 수를 더한 결과를 return해주는 클래스
+
+//class calc{
+//	static int sum(int num1, int num2) {
+//		return num1 + num2;
+//	}
+//}
+//public class C20230704{
+//	public static void main(String[] args) {
+//		
+//		Scanner sc =new Scanner(System.in);
+//		System.out.println("정수 2개를 입력해주세요");
+//		int num1 = sc.nextInt();
+//		int num2 = sc.nextInt();
+//		
+//		System.out.println("정수의 합 : " + calc.sum(num1, num2));
+//		
+//		sc.close();
+//		
+//	}
+//}
