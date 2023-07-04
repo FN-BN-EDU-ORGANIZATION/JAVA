97df5c91a9fb64cd6c4ed2f488f03613d28056c1
woo0121 <131091433+woo0121@users.noreply.github.com>
2023-07-04 15:51:21 +0900

    Add files via upload

---
+++ src/C20230629/C20230704.java
@@ -0,0 +1,30 @@
+package C20230629;
+
+//public class C20230704 {
+//	class good {
+//	    public static void main(String[] args) {
+//	        int[] a = new int[8];
+//	        int i = 0;
+//	        int n = 10;
+//	        while (n > 0) {                      // 코드 라인 A: n의 값이 0보다 클 때까지 반복
+//	            a[i++] = n % 2;                   // 코드 라인 B: n을 2로 나눈 나머지를 a[i]에 할당
+//	            n /= 2;                           // n을 2로 나누어 업데이트
+//	        }
+//	        for (i = 7; i >= 0; i--) {
+//	            System.out.print(a[i]);           // 배열 a의 요소를 역순으로 출력
+//	        }
+//	    }
+//	}
+//}
+//public class Exam {
+//	  public static void main(String[] args){
+//	  
+//	    int a = 0;				//변수 a 0으로 초기화
+//	    for(int i=1; i<999; i++){			//i를 1부터 998까지 반복
+//	      if(i%3==0 && i%2!=0)			//i가 3으로 나누어 떨어지고 2로 나누엇을때 0이 아니라면 아래코드 실행 
+//	        a = i;
+//	    }
+//	    System.out.print(a);
+//	  }
+//	}	
+//		
