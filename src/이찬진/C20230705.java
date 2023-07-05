f6381ece8ec316b6ebb42a31899f58d640c547da
chanj <chanjini7@naver.com>
2023-07-05 15:43:50 +0900

    20230705 - 이찬진

---
+++ src/이찬진/C20230705.java
@@ -0,0 +1,36 @@
+package 이찬진;
+
+public class C20230705 {
+
+	public static void main(String[] args) {
+
+		        // 정리 : 손님들이 치킨을 주문하는 상황을 반복문과 break문을 이용해 문제 풀이
+				
+				//예제1
+		        // for문
+//		        int max = 20; // 최대 판매 가능한 치킨 수
+//		        for (int i = 1; i <= 50; i++) { // 1부터 50까지 반복
+//		            System.out.println(i + "번 손님, 주문하신 치킨 나왔습니다."); // 현재 손님 번호와 함께 출력
+//		            if (i == max) { // 만약 현재 손님 번호가 최대 판매 가능한 치킨 수와 같다면
+//		                System.out.println("금일 재료가 모두 소진되었습니다."); // 재료 소진 메시지 출력
+//		                break; // 반복문을 탈출하여 종료
+//		            }
+//		        }
+//		        System.out.println("영업을 종료합니다.");
+
+		        //예제2
+		         //while문
+//		        int index = 1; // 손님 번호 초기화
+//		        int max = 20; // 최대 판매 가능한 치킨 수
+//		        while (index <= 50) { // 손님 번호가 50보다 작거나 같을 때까지 반복
+//		            System.out.println(index + "번 손님, 주문하신 치킨 나왔습니다."); // 현재 손님 번호와 함께 출력
+//		            if (index == max) { // 만약 현재 손님 번호가 최대 판매 가능한 치킨 수와 같다면
+//		                System.out.println("금일 재료가 모두 소진되었습니다."); // 재료 소진 메시지 출력
+//		                break; // 반복문을 탈출하여 종료
+//		            }
+//		            index++; // 손님 번호 증가
+//		        }
+//		        System.out.println("영업을 종료합니다.");
+//		
+		    }
+}
