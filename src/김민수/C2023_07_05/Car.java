e8e84d2dc49b6e79ae624970a601f7075378f85b
minsukims <thdgodakfgod1@naver.com>
2023-07-05 22:55:57 +0900

    7월5일 오늘까지만 예제 2가지

---
+++ src/김민수/C2023_07_05/Car.java
@@ -0,0 +1,5 @@
+package 김민수.C2023_07_05;
+
+public class Car {
+
+}
---
+++ src/김민수/C2023_07_05/VeryImportantPerson.java
@@ -0,0 +1,10 @@
+package 김민수.C2023_07_05;
+
+final class Member{ 
+	
+}
+//final을 붙이면 상속이 제한되면서 오버라이딩이 제한된다.
+//대신 다른 클래스의 기능과는 차이가 없다.
+//public class VeryImportantPerson extends Member{ //클래스에 final이 붙으면 상속이 안됨.
+//
+//}
