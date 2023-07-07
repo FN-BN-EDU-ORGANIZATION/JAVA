334d4382285936a86b03355d3281631041e8472f
YoonGwngHyuck <YGH@YGH-Laptop>
2023-07-07 16:03:00 +0900

    0707

---
+++ src/윤광혁/예제20230707.java
@@ -0,0 +1,26 @@
+package 윤광혁;
+
+public class 예제20230707 {
+	//1 람다 표현식
+	//	() => {}
+	//람다 표현식은 자바 8에서 도입된 함수형 프로그래밍의 기능중 하나이다.
+	//람다 표현식은 익명 함수를 간단하게 표현하는 방법으로 코드의 간결성과 가독성을 향상시킨다.
+	
+	//() 는 메서드의 매개변수를 나타낸다.
+	//=> 는 람다 표현식의 구분자이다.
+	//{} 안에는 실행할 코드 블록을 작성한다.
+	
+	//2 람다 표현식 예제 분석
+//	List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+//
+//	numbers.forEach((Integer number) -> {
+//	    System.out.println(number);
+//	});
+	
+	//위 코드에서 numbers 에 리스트로 숫자를 넣었습니다.
+	//numbers.forEach()메서드는 리스트의 각요소를 인자로 받은 람다 표현식입니다.
+	//표현식은 매개변수 Integer number 로 리스트의 각요소를 하나씩 받아
+	//{System.out.println(number)}으로 출력합니다.
+	
+	
+}
