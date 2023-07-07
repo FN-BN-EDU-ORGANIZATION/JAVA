92aff1af16351b352643068e4fd32c276b198050
LEEJUHYUN <free21004@naver.com>
2023-07-07 15:52:26 +0900

    7월 7일 공부내용

---
+++ src/LEEJUHYUN/C20230707.java
@@ -0,0 +1,36 @@
+package LEEJUHYUN;
+
+public class C20230707 {
+
+		//메소드 개념 정리
+//		자바에서 클래스는 멤버(member)로 속성을 표현하는 필드(field)와 기능을 표현하는 메소드(method)를 가진다.
+//		그중에서 메소드(method)란 어떠한 특정 작업을 수행하기 위한 명령문의 집합이라 할 수 있다.
+//		클래스에서 메소드를 작성하여 사용하는 이유는 중복되는 코드의 반복적인 프로그래밍을 피할 수 있기 때문이다.
+//		또한, 모듈화로 인해 코드의 가독성도 좋아지며 프로그램에 문제가 발생하거나 기능의 변경이 필요할 때도 손쉽게 유지보수를 할 수 있게 된다.
+	
+		
+	    // "toUpper" 메소드 정의
+//	    public static String toUpper(String text) {
+//	        // "text" 문자열을 대문자로 변환하여 "result" 변수에 저장
+//	        String result = text.toUpperCase();
+//	        // 변환된 문자열 반환
+//	        return result;
+//	    }
+//
+//	    public static void main(String[] args) {
+//	        // "안녕하세요" 문자열을 "greeting" 변수에 할당
+//	        String greeting = "안녕하세요";
+//	        // "toUpper" 메소드 호출하여 대문자로 변환된 문자열을 "uppercaseGreeting" 변수에 할당
+//	        String uppercaseGreeting = toUpper(greeting);
+//	        // "uppercaseGreeting" 출력
+//	        System.out.println(uppercaseGreeting);
+//	    }
+	    
+	    //정리 : 위의 예제는 클래스 내에 두 개의 메소드로 구성된다.
+//	    1. toUpper 메소드: 이 메소드는 String 타입의 text 인자를 받아 해당 문자열을 대문자로 변환하여 반환한다.
+//	    2. main 메소드: 이 메소드는 프로그램의 시작점이다. "안녕하세요"라는 문자열을 greeting 변수에 할당하고, toUpper 메소드를 호출하여 greeting을 대문자로 변환한 문자열을 uppercaseGreeting 변수에 할당한다. 
+//	    마지막으로, uppercaseGreeting 변수의 값을 출력한다.
+//	    
+//	    위의 코드는 "안녕하세요"라는 문자열을 대문자로 변환하여 출력하는 예제 풀이이다. 
+//	    toUpper 메소드를 사용하여 문자열을 변환하고, 이를 main 메소드에서 호출하여 결과를 출력하는 방법을 보여준다.
+	}
