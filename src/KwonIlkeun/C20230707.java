8f668b0ba786512812b5a63e5b0bedaa433c5bfd
kwonkk <ig1108@naver.com>
2023-07-10 00:27:55 +0900

    20230707

---
+++ src/KwonIlkeun/C20230707.java
@@ -0,0 +1,36 @@
+package KwonIlkeun;
+
+import java.util.Scanner;
+
+public class C20230707 {
+
+	public static void main(String[] args) {
+		
+		
+		//1. 주급계산하기(근무시간과 시간당 급여액을 입력받아 주급을 계산하는 프로그램 작성)
+//		int hour,pay,week_pay;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("하루 근무시간: ");
+//		hour = sc.nextInt();
+//		
+//		System.out.println("시간당 급여액: ");
+//		pay = sc.nextInt();
+//		
+//		week_pay = hour*pay*7;
+//		
+//		System.out.println("주급: "+week_pay);
+		
+		
+		//2. 서울에서 부산까지 시속 80km로 달릴 경우 몇시간 몇분이 걸리는지 계산(거리 약 430km)
+//		int hour;
+//		int min;
+//		
+//		hour = 430/80;
+//		min = 430%80;
+//		
+//		System.out.println("시간: "+hour);
+//		System.out.println("분: "+min);
+	}
+
+}
