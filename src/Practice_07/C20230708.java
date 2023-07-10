367b1f758b7ae5d7096caefd96b22b072b509c3e
FORYOUCOM <FORYOUCOM@dd>
2023-07-10 22:44:59 +0900

    ㅁㅁ

---
+++ src/Practice_07/C20230708.java
@@ -0,0 +1,83 @@
+package Practice_07;
+
+
+
+
+public class C20230708 {
+	public static void main(String[] args) {
+
+		//의문점		: X
+		//정리 : Java Script 복습, 프로젝트 복습
+
+//			
+//		
+		//예제 1
+		 
+//		조건문
+// 		1. if문
+//		if(조건식){실행 코드 블럭}, 다양한 조건다쓰임
+//		2. switch
+//		조건값에 따라 수행블럭을 구분해서 정의, 참거짓연산가능
+//		switch(입력변수){case 조건값 : 실행;break;
+//		default : 기본실행;}
+//		-> 입력변수는 정수, 문자
+//		-> 마지막 break 넣어야됨 필수 안넣을시 이후 조건의 구문들 실행됨
+//		-> default 조건에 해당하는 입력값이 없을 경우 실행되는 코드 기술
+		
+
+//		public class Conditional {
+//	        void login() {
+//	            Scanner scan = new Scanner(System.in);
+//
+//	            System.out.print("## 아이디를 입력하세요: ");		
+//	            String uname = scan.next();
+//
+//	            System.out.print("# 비밀번호를 입력하세요: ");
+//	            String pwd = scan.next();
+//
+//	            if(uname.equals("hong") && pwd.equals("1234")) {
+//	            System.out.println("인증 확인!! -> 로그인 성공");
+//	            }
+//	            else {
+//	            System.out.println("아이디나 비밀번호가 틀렸습니다.!!");
+//	            }
+//	        }
+//			로그인, 쪽지확인, 종료 중 선택하면 아이디 입력 -> 비밀번호 -> 메뉴선택
+//			-> 인증확인 -> 로그인 성공 순으로 진행
+//			아이디, 비번 공백없이 저장
+//	    void check() {
+//	        int cnt = 10;
+//	        String msg = cnt > 0? "새로운 쪽지가 있습니다.!!" : "새로운 쪽지가 없습니다.!!";
+//	        System.out.println(msg);
+//	    }
+//
+//	    public static void main(String[] args) {
+//	        Conditional con = new Conditional();
+//
+//	        while(true) {
+//	            System.out.printf("# 메뉴를 선택하세요 (1:로그인, 2:쪽지확인, x:종료) ==> ");
+//	            Scanner scan = new Scanner(System.in);
+//	            String sel = scan.next();
+//
+//	            switch(sel) {
+//	            case "1": con.login();break;
+//	            case "2": con.check();break;
+//	            case "x": System.exit(0);
+//	            default : System.out.println("잘못된 입력 입니다.!!");
+//	            }	
+//	        }
+//	    }
+//	}			
+//		-> switch 문을 사용하여 계산 수행
+//		-> Scan 해서 저장
+//				
+//		       
+		
+
+
+		
+		
+	}	
+		
+	
+}
