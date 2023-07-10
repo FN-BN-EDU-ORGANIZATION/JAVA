1ded89be72f3622fa613708c58241b56d3be2cd6
chanj <chanjini7@naver.com>
2023-07-10 15:54:08 +0900

    20230710

---
+++ src/이찬진/C20230710.java
@@ -0,0 +1,62 @@
+package 이찬진;
+
+import java.util.Scanner;
+
+public class C20230710 {
+
+	public static void main(String[] args) {
+		
+		//예제1
+		//100 이하의 정수를 입력받아서 for문을 사용하지 않고 메서드만을 활용하여
+		//1부터 입력받은 수까지의 합을 구하는 코드를 작성하시오
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		do {
+//			System.out.println("100이하의 정수를 입력하세요 : ");
+//			int in = sc.nextInt();
+//			
+//			if(in > 100) {
+//				System.out.println(" 큰 값을 입력했습니다.");	
+//			}
+//			else if(in == 0) {
+//				System.out.println("종료합니다.");
+//				break;
+//			}
+//			else {
+//				System.out.println(sum(in));
+//			}
+//		}while(true);
+//
+//	}
+//
+//	private static int sum(int in) {
+//		int ret;
+//		
+//		if(in >0) {
+//			ret = in + sum((in -1));
+//		}
+//		else {
+//			ret =0;
+//		}
+//		return ret;
+//	}
+//
+//}
+		
+		//예제2
+		//입력받은 수가 양수인지 음수인지 판단하는 프로그램을 작성하세요
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("정수를 입력하세요");
+//		int num = sc.nextInt();
+//		
+//		if(num>0) {
+//			System.out.printf("%d는 양수입니다. ", num);			
+//		}
+//		else {
+//			System.out.printf("%d는 음수입니다. ", num);
+//		}
+//		
+	}
+}
