8b0e17a5133dfdfc66f312ac1b1580eaf2cd537d
chung2code <anthdny7@gmail.com>
2023-07-12 01:53:25 +0900

    230711

--- src/Songjaeuk/D230705/Exam01.java
+++ src/Songjaeuk/D230705/Exam01.java
@@ -1,5 +1,74 @@
 package Songjaeuk.D230705;
 
+
+
+// 추상 클래스
+abstract class Shape {
+    private String color;
+
+    // 생성자
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    // 추상 메서드: 면적을 계산하는 기능을 구현하는 메서드
+    public abstract double getArea();
+
+    // 일반 메서드: 색상을 반환하는 메서드
+    public String getColor() {
+        return color;
+    }
+}
+
+// Circle 클래스: Shape 클래스를 상속받음
+class Circle extends Shape {
+    private double radius;
+
+    // 생성자
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    // 추상 메서드 구현
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Rectangle 클래스: Shape 클래스를 상속받음
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    // 생성자
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    // 추상 메서드 구현
+    public double getArea() {
+        return width * height;
+    }
+}
+
 public class Exam01 {
+
+	// 메인 클래스
+	
+	    public static void main(String[] args) {
+	        // Circle 객체 생성
+	        Circle circle = new Circle("Red", 2.0);
+	        System.out.println("Circle Area: " + circle.getArea()); // 동그라미의 면적 출력
+	        System.out.println("Circle Color: " + circle.getColor()); // 동그라미의 색상 출력
+
+	        // Rectangle 객체 생성
+	        Rectangle rectangle = new Rectangle("Blue", 3.0, 4.0);
+	        System.out.println("Rectangle Area: " + rectangle.getArea()); // 사각형의 면적 출력
+	        System.out.println("Rectangle Color: " + rectangle.getColor()); // 사각형의 색상 출력
+	    }
+	}
 
 }
--- src/Songjaeuk/D230705/Exam02.java
+++ src/Songjaeuk/D230705/Exam02.java
@@ -1,5 +1,36 @@
 package Songjaeuk.D230705;
 
-public class Exam02 {
+	class Ball {		//Ball에 숫자를 넣음
+    private int number;
+    public Ball(int number){
+        this.number= number;
 
+    }
+    public int getNumber(){
+        return number;
+
+    }
 }
+
+
+public abstract class Exam02 {
+
+	/*
+	1.1~45까지 써있는 Ball을 로또 기계에 넣는디
+	2.로또기계에 있는 Ball들을 섞는다.
+	3.섞인 Ball중에서 6개를 꺼낸다.
+	 */
+	
+	    //인터페이스가 가지고 있는 메소드는 모두 추상 메소드이기 때문에 abstract가 생략가능
+
+
+	    int Max_BALL_COUNT = 45;
+	    int RETURN_BALL_COUNT = 6;
+	    public abstract void setBalls(Ball[]balls);  //Ball[] Ball이 여러개를 받겠다.
+	    public abstract void mix(); //자기가 가지고 있는 Ball들을 섞는다.
+	    public abstract Ball[] getball(); //6개의 Ball을 반환한다.
+
+	}
+
+
+
