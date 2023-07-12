9c307900047e1563ed8decd8d7a60b4d7c768495
mirdayo <mir6066@gmail.com>
2023-07-12 12:07:58 +0900

    230712 Java

--- src/C202307/C230712.java
+++ src/C202307/C230712.java
@@ -1,48 +1,29 @@
 package C202307;
 
-interface Predator {
-	String bark();
-}
-
-class Animal {
-    public String hello() {
-        return "hello";
-    }
-}
-
-class Dog extends Animal {
-}
-
-class Lion extends Animal implements Predator {
-    public String bark() {
-        return "Bark bark!!";
-    }
-}
+import java.util.Arrays;
 
 public class C230712 {
 
 	public static void main(String[] args) {
-//		  Animal a = new Animal();  // 1번
-//	      Animal b = new Dog();  // 2번
-//	      Animal c = new Lion();  // 3번
-//	      Dog d = new Animal();  // 4번 오류발생
-//	      Predator e = new Lion();  // 5번
-	      //Dog 클래스는 Animal의 자식 클래스로 IS-A 관계가 성립될 수 없기 때문 나머지는 모두 IS-A 관계가 성립
-	      
-	      Animal a = new Lion();
-	      Lion b = new Lion();
-	      Predator c = new Lion();
-
-	      System.out.println(a.hello());  // 1번
-	      System.out.println(a.bark());   // 2번 오류발생
-	      System.out.println(b.hello());  // 3번
-	      System.out.println(b.bark());   // 4번
-	      System.out.println(c.hello());  // 5번 오류발생
-	      System.out.println(c.bark());   // 6번
-	      //2번 문장과 5번 문장에서 오류가 발생한다. 2번 문장이 오류가 발생하는 이유는 
-	      //a 객체가 Animal 타입으로 생성되었기 때문이다. Animal 타입의 객체는 hello 메서드만 사용이 가능
-	      //5번 문장이 오류가 발생하는 이유는 c 객체가 Predator 타입으로 생성되었기 때문
-	      //Predator 타입의 객체는 bark 메서드만 사용이 가능
-	      
+		//홀수에만 2를 곱하여 리턴하기
+		int[] numbers = {1, 2, 3, 4, 5};
+        int[] result = Arrays.stream(numbers)  // IntStream 생성한다
+                .filter((a) -> a % 2 == 1)  // 홀수만 취합한다
+                .map((a) -> a * 2)  // 각 항목에 2를 곱한다
+                .toArray()  // int[] 배열로 반환한다.
+                ;
+	    for (int num : result) {
+	        System.out.print(num + " ");
 	    }
-    }
+	    System.out.println();
+        //음수 제거하기
+        int[] numbers2 = {1, -2, 3, -5, 8, -3};
+        int[] result2 = Arrays.stream(numbers2)  // IntStream을 생성한다
+                .filter((a) -> a >= 0)  // 음수를 제거한다.
+                .toArray()  // int[] 배열로 반환한다.
+                ;
+	    for (int num : result2) {
+	        System.out.print(num + " ");
+	    }
+	}
+}
