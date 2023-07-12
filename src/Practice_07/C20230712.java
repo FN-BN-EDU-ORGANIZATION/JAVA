7e3f2e8c67fc40c3983b1f414992e7a442489ec8
FORYOUCOM <FORYOUCOM@dd>
2023-07-12 23:24:06 +0900

    ㅁ

---
+++ src/Practice_07/C20230712.java
@@ -0,0 +1,65 @@
+package Practice_07;
+
+
+
+
+public class C20230712 {
+	public static void main(String[] args) {
+
+		//의문점		: X
+		//정리 : Java Script 복습, 프로젝트 복습
+		
+		
+//		상속
+//		객체의 필드와 메소드를 다른 객체가 물려받을 수 있는 기능
+//		상속 클래스 : 부모 클래스, 상위클래스, 기초클래스
+//		상속받는 클래스 : 자식클래스, 하위 클래스, 유도클래스
+		
+//			
+//		
+		//예제 1
+		 
+//		public class Beer{
+//		public static void main(String[] args) {
+//			Beer lemon = new Beer("맥주","레몬,3")		
+//		}
+//		맥주 레몬맛 객체 생성
+//		
+//		System.out.println("타입 : "+beer.getType());
+//		System.out.println("도수 : "+beer.getABV());
+//		Beer클래스로부터 상속받는 필드
+//		
+//		System.out.println("맛 : "+Beer.getFlavor());
+//		Beer로부터 상속받는 필드
+//		
+//		Beer.flavordrink();
+		
+		
+//		예제 2
+//		class Neuron {
+//	    Random random = new Random();
+//	    private Double bias = random.nextDouble(-1, 1); 
+//	    public Double weight1 = random.nextDouble(-1, 1); 
+//	    private Double weight2 = random.nextDouble(-1, 1);
+//	   
+//	    public double compute(double input1, double input2){
+//	    double preActivation = (this.weight1 * input1) + (this.weight2 * input2) + this.bias;
+//	      double output = Util.sigmoid(preActivation);
+//	      return output;
+//	    }
+//	  }
+//		-1에서 1사이 무작위 실수로 초기화
+//		뉴런 출력 계산할때 각 입력 가중치 곱하고 더함
+//		처리되지않는 것은 preActivation 저장
+//		시그모이드 활성화 함수 사용
+//		뉴론클래스를 모델링 하여 신경망을 만드는 과정중하나(솔지키 다읽어봤는데 모르겠다)
+	    
+		
+
+
+		
+		
+	}	
+		
+	
+}
