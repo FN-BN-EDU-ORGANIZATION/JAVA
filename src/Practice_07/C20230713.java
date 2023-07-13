fd82d9bfd3843b58be0e6570cd9a7be03b993251
FORYOUCOM <FORYOUCOM@dd>
2023-07-13 22:36:48 +0900

    ㅁ

---
+++ src/Practice_07/C20230713.java
@@ -0,0 +1,83 @@
+package Practice_07;
+
+
+
+
+public class C20230713 {
+	public static void main(String[] args) {
+
+		//의문점		: X
+		//정리 : Java Script 복습, 프로젝트 복습
+		
+//		import java.util.Random;
+//
+//		public class RandomNumberProbability {
+//		    public static void main(String[] args) {
+//		        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//		        double[] probabilities = calculateProbabilities(numbers);
+//		        
+//		        for (int i = 0; i < numbers.length; i++) {
+//		            System.out.println("Number: " + numbers[i] + ", Probability: " + probabilities[i]);
+//		        }
+//		    }
+//			probabilities를 활용하여 확률 저장
+//			totalNumbers로 배열의 길이 저장
+//		 	
+//		    
+//		    public static double[] calculateProbabilities(int[] numbers) {
+//		        int totalNumbers = numbers.length;
+//		        double[] probabilities = new double[totalNumbers];
+//		        double totalProbability = 0.0;
+//		        
+//		        Random random = new Random();
+//		        
+//		        for (int i = 0; i < totalNumbers; i++) {
+//		            probabilities[i] = random.nextDouble();
+//		            totalProbability += probabilities[i];
+//		        }
+//		        
+//		        for (int i = 0; i < totalNumbers; i++) {
+//		            probabilities[i] /= totalProbability;
+//		        }
+//		        
+//		        return probabilities;
+//		    }
+//		}
+//			for로 무한을 6자리 숫자돌려서 0과 1사이의 무작위 숫자 저장하여
+//			누적 전체 확률 계산
+//			다시 for로 숫자 합 계산
+//			최종적으로 probabilities에 반환
+//			1부터 9까지 무작위 선택
+
+//	int number = scanner.nextInt();
+//	        
+//	        String message;
+//	        
+//	        switch (number) {
+//	            case 1:
+//	                message = "일";
+//	                break;
+//	            case 2:
+//	                message = "이";
+//	                break;
+//	            case 3:
+//	                message = "삼";
+//	                break;
+//	            case 4:
+//	                message = "사";
+//	                break;
+//	            case 5:
+//	                message = "오";
+//	                break;
+//	            default:
+//	                message = "잘못된 입력입니다.";
+//	                break;
+//	        }
+//	
+//			switch 문을 사용하여 입력받는 number 변수의 값에 따라 
+//			저장
+		
+	}	
+		
+	
+}
